4272678deb431dec4a4580847b6d3130158ec197
gerrygeop <gerryja28@gmail.com>
2025-05-28 09:34:30 +0800

    feat: add components

---
+++ src/components/admin/Sidebar.vue
@@ -0,0 +1,33 @@
+<template>
+    <aside class="w-64 bg-white shadow-lg">
+        <div class="p-6 border-b border-gray-100">
+            <h1 class="text-2xl font-bold text-blue-600 flex items-center">TickTrack</h1>
+        </div>
+        <nav class="mt-6">
+            <RouterLink
+                :to="{ name: 'admin.dashboard' }"
+                class="flex items-center px-6 py-3 text-gray-600 hover:bg-gray-50 hover:border-l-4 hover:border-gray-200"
+                :class="{
+                    'bg-blue-50 border-l-4 border-blue-600': $route.name === 'admin.dashboard',
+                }"
+            >
+                Dashboard
+            </RouterLink>
+            <RouterLink
+                :to="{ name: 'admin.tiket' }"
+                class="flex items-center px-6 py-3 text-gray-600 hover:bg-gray-50 hover:border-l-4 hover:border-gray-200"
+                :class="{
+                    'bg-blue-50 border-l-4 border-blue-600': $route.name === 'admin.tiket',
+                }"
+            >
+                Tiket
+            </RouterLink>
+            <a
+                href="#"
+                class="flex items-center px-6 py-3 text-gray-600 hover:bg-gray-50 hover:border-l-4 hover:border-gray-200 mt-6"
+            >
+                Dashboard
+            </a>
+        </nav>
+    </aside>
+</template>
